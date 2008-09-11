118bf2bdeec21a601593114a7ada3b2b748d33b5
dave@addedbytes.com <dave@addedbytes.com@a8456d92-7fe5-11dd-a170-e1db062c8548>
2008-09-11 12:30:23 +0000

    Replaced tabs with spaces in older userscripts.

--- trunk/number_google_results.user.js
+++ trunk/number_google_results.user.js
@@ -4,7 +4,7 @@
 //
 // ==UserScript==
 // @name          Number Google Results
-// @description	  Numbers results of Google searches
+// @description   Numbers results of Google searches
 // @include       http://google.*
 // @include       http://www.google.*
 // ==/UserScript==
@@ -13,56 +13,56 @@
 
 (function() {
 
-	function q(key) {
-		var value = null;
-		for (var i = 0; i < q.keys.length; i++) {
-			if (q.keys[i] == key) {
-				value = q.values[i];
-				break;
-			}
-		}
-		return value;
-	}
-	
-	q.keys = new Array();
-	q.values = new Array();
+    function q(key) {
+        var value = null;
+        for (var i = 0; i < q.keys.length; i++) {
+            if (q.keys[i] == key) {
+                value = q.values[i];
+                break;
+            }
+        }
+        return value;
+    }
+    
+    q.keys = new Array();
+    q.values = new Array();
 
-	function q_Parse() {
-		var query = window.location.search.substring(1);
-		var pairs=query.split('&');
-		for (var i = 0 ; i < pairs.length; i++) {
-			var pos = pairs[i].indexOf('=');
-			if (pos >= 0) {
-				var a = pairs[i].substring(0,pos);
-				var value = pairs[i].substring(pos+1);
-				q.keys[q.keys.length] = a;
-				q.values[q.values.length] = value;
-			}
-		}
-	}
-	
-	q_Parse();
-	var j = 0;
-	var s = q('start');
+    function q_Parse() {
+        var query = window.location.search.substring(1);
+        var pairs=query.split('&');
+        for (var i = 0 ; i < pairs.length; i++) {
+            var pos = pairs[i].indexOf('=');
+            if (pos >= 0) {
+                var a = pairs[i].substring(0,pos);
+                var value = pairs[i].substring(pos+1);
+                q.keys[q.keys.length] = a;
+                q.values[q.values.length] = value;
+            }
+        }
+    }
+    
+    q_Parse();
+    var j = 0;
+    var s = q('start');
 
-	if (s == null) {
-		s=0;
-	}
+    if (s == null) {
+        s=0;
+    }
 
-	s = parseInt(s) + 1;
+    s = parseInt(s) + 1;
 
-	function n(){
-		ps = document.getElementsByTagName('*');
-		for (pi = 0; pi < ps.length; pi++) {
-			tp = ps[pi];
-			if (tp.className == 'g') {
-				w = s + j;
-				tp.innerHTML = w + '. ' + tp.innerHTML;
-				j++;
-			}
-		}
-	}
-	
-	window.addEventListener("load", n(), false);
+    function n(){
+        ps = document.getElementsByTagName('*');
+        for (pi = 0; pi < ps.length; pi++) {
+            tp = ps[pi];
+            if (tp.className == 'g') {
+                w = s + j;
+                tp.innerHTML = w + '. ' + tp.innerHTML;
+                j++;
+            }
+        }
+    }
+    
+    window.addEventListener("load", n(), false);
 
 })();
